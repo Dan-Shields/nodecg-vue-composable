229d5faa4ba4993a32518e1ecad675bf8f24f0b8
Daniel Shields <me@danielshields.uk>
2022-02-07 16:46:42 +0000

    Rewrite in TS and update method names

---
+++ index.ts
@@ -0,0 +1,142 @@
+import { ref, watch, isRef, reactive, computed, readonly, Ref, DeepReadonly } from 'vue-demi'
+import { ReplicantOptions } from 'nodecg-types/types/browser'
+import clone from 'clone'
+
+interface ReactiveReplicant<T> {
+    data: T | undefined,
+    oldData: DeepReadonly<T | undefined>,
+    changed: boolean,
+    save: () => void,
+    revert: () => void,
+    loadDefault: () => void
+}
+
+export function useReplicant<T>(name: string, namespace: string | undefined, opts: ReplicantOptions<T> = {}) {
+    return useReplicantRaw(name, namespace, opts)?.reactiveReplicant
+}
+
+function useReplicantRaw<T>(name: string, namespace: string | ReplicantOptions<T> | undefined, opts: ReplicantOptions<T> | undefined) {
+	if (isRef(name)) {
+		console.warn(`Tried to create a StaticReplicant using a reactive name (${name.value})`)
+		return null
+	}
+
+	if (opts === undefined && typeof namespace !== 'string') {
+		opts = namespace;
+	}
+
+    let rep = typeof namespace === 'string' ? nodecg.Replicant<T>(name, namespace, opts) : nodecg.Replicant<T>(name, opts)
+
+    // TODO: work out why this assertion is necessary
+    const newVal = ref(clone(opts?.defaultValue)) as Ref<T | undefined>
+    const oldVal = ref(clone(opts?.defaultValue)) as Ref<T | undefined>
+    const changed = ref(false)
+    const upToDate = ref(true)
+
+    function checkChanged() {
+        changed.value = JSON.stringify(newVal.value) !== JSON.stringify(oldVal.value)
+    }
+    
+    watch(newVal, checkChanged, { deep: true })
+
+    const listener = (newRepVal: T | undefined, oldRepVal: T | undefined) => {
+        oldVal.value = clone(newRepVal)
+
+        if (!oldRepVal) changed.value = false
+                
+        if (changed.value) {
+            checkChanged()
+            upToDate.value = !changed.value
+        } else {
+            newVal.value = clone(oldVal.value)
+            upToDate.value = true
+        }
+    }
+
+    rep.on('change', listener)
+
+    function save() {
+        rep.value = newVal.value
+    }
+    function revert() {
+        newVal.value = clone(oldVal.value)
+    }
+    function loadDefault() {
+        newVal.value = clone(opts?.defaultValue)
+    }
+
+    const reactiveReplicant: ReactiveReplicant<T> = reactive({
+        data: newVal,
+        oldData: readonly(oldVal),
+        changed,
+        save,
+        revert,
+        loadDefault
+    })
+
+    return {
+        reactiveReplicant,
+        listener
+    }
+}
+
+export function useDynamicReplicant<T>(name: Ref<string>, namespace: string, opts: ReplicantOptions<T> | undefined) {
+	if (!isRef(name)) {
+		console.warn(`Tried to create a DynamicReplicant using a static name (${name})`)
+		return null
+	}
+
+    const repRef: Ref<{ reactiveReplicant: ReactiveReplicant<T>, listener: (newRepVal: T | undefined, oldRepVal: T | undefined) => void } | null> = ref(null)
+
+    function setReplicant(oldName?: string) {
+        // remove old listeners when we change the name to prevent potential memory leaks
+        if (oldName && repRef.value) nodecg.Replicant(oldName).removeListener('change', repRef.value.listener)
+
+        if (!(typeof name.value === 'string' || typeof name.value === 'number')) {
+            repRef.value = null
+        } else {
+            repRef.value = useReplicantRaw<T>(name.value, namespace, opts)
+        }
+    }
+
+    setReplicant()
+    watch(name, (_, oldName) => {
+        setReplicant(oldName)
+    })
+
+    return computed(() => repRef.value?.reactiveReplicant)
+}
+
+export interface Asset {
+    base: string,
+    namespace: string,
+    category: string,
+    ext: string,
+    name: string,
+    sum: string,
+    url: string
+}
+
+export function useAssetReplicant(name: string, namespace: string) {
+    const rep = nodecg.Replicant<Asset[]>(`assets:${name}`, namespace)
+    const newVal: Ref<Asset[]> = ref([])
+
+    rep.on('change', (newRepVal: Asset[] | null | undefined) => {
+        newVal.value = clone(newRepVal ?? [])
+    })
+
+    return newVal
+}
+
+/**
+ * @deprecated Renamed to useReplicant
+ */
+export const ReactiveReplicant = useReplicant
+/**
+ * @deprecated Renamed to useDynamicReplicant
+ */
+export const DynamicReactiveReplicant = useDynamicReplicant
+/**
+ * @deprecated Renamed to useAssetReplicant
+ */
+export const AssetReplicant = useAssetReplicant
